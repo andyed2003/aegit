cf7ed9d45af4951e4d5976bbc72539785acea14a
andy <andy@andy-A15A-A15HE>
2013-09-13 15:15:59 +0100

    Latest AtomicityD model, tele-commit

---
+++ Exploratory/AtomicityDec3/src/controllerImpl.h
@@ -0,0 +1,6 @@
+#ifndef CONTROLLERIMPL_H
+#define CONTROLLERIMPL_H
+void controllerImpl_controller_fmiDoStep();
+void controllerImpl_fmiSetInteger(int p);
+#endif
+
---
+++ Exploratory/AtomicityDec3/src/environImpl.c
@@ -0,0 +1,29 @@
+#include "Common.h"
+// Protected: environImpl
+
+// Variables and constants
+int b_environImpl = 99;
+
+
+// Subroutines
+void environImpl_env_fmiDoStep()
+{
+// [Internal] This will block until the guard has been met (no nice way of doing this in OpenMP 2.5)
+BOOL completed = FALSE; 	 // [Internal] Private to the thread by default
+// [Internal] Try and get lock
+// Translated code
+b_environImpl = (b_environImpl + 1);
+// [Internal] Set completed flag
+completed = TRUE;
+}
+
+void environImpl_fmiGetInteger(int *b)
+{
+// [Internal] This will block until the guard has been met (no nice way of doing this in OpenMP 2.5)
+BOOL completed = FALSE; 	 // [Internal] Private to the thread by default
+// [Internal] Try and get lock
+// Translated code
+// [Internal] Set completed flag
+completed = TRUE;
+}
+
