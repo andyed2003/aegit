8824604106ac04e92da154cc3d9390dabb7b9e72
andy <andy@andy-A15A-A15HE>
2013-09-20 16:39:53 +0100

    Added fmiDoStep subroutine to the IL1 representation (in the protected object) of the FMU Machine.

--- ac.soton.fmusim.codegen/src/ac/soton/fmusim/codegen/translator/FMUMachineTaskingTranslator.java
+++ ac.soton.fmusim.codegen/src/ac/soton/fmusim/codegen/translator/FMUMachineTaskingTranslator.java
@@ -5,19 +5,28 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.ui.statushandlers.StatusManager;
 import org.eventb.codegen.il1.IL1Element;
 import org.eventb.codegen.il1.Il1Factory;
 import org.eventb.codegen.il1.Protected;
 import org.eventb.codegen.il1.Subroutine;
+import org.eventb.codegen.il1.Command;
+import org.eventb.codegen.il1.Task;
 import org.eventb.codegen.il1.translator.IL1TranslationManager;
 import org.eventb.codegen.tasking.AbstractTaskingTranslator;
+import org.eventb.codegen.tasking.CodeGenTasking;
 import org.eventb.codegen.tasking.TaskingTranslationException;
 import org.eventb.codegen.tasking.TaskingTranslationManager;
 import org.eventb.codegen.tasking.TaskingTranslationUnhandledTypeException;
+import org.eventb.emf.core.AbstractExtension;
 import org.eventb.emf.core.EventBElement;
 import org.eventb.emf.core.machine.Event;
 import org.eventb.emf.core.machine.impl.MachineImpl;
+
+import compositeControl.CompositeControl;
 
 import tasking.FMU_Machine;
 
@@ -30,7 +39,8 @@ public class FMUMachineTaskingTranslator extends AbstractTaskingTranslator {
 
 		MachineImpl containingMachine = (MachineImpl) source.eContainer();
 		// This is where we create protected object from the FMU
-		Protected protectedObj = Il1Factory.eINSTANCE.createProtected();;
+		Protected protectedObj = Il1Factory.eINSTANCE.createProtected();
+		;
 		// Translate the FMU as a shared machine
 		// Get the events
 		EList<Event> events = containingMachine.getEvents();
@@ -48,7 +58,8 @@ public class FMUMachineTaskingTranslator extends AbstractTaskingTranslator {
 			// for each of the events look for the corresponding translation.
 			// and add it to the list.
 			for (Event evt : events) {
-				// find the event/subroutine & machine names in the key of stored element.
+				// find the event/subroutine & machine names in the key of
+				// stored element.
 				if (nxt.contains(evt.getName())
 						&& nxt.contains(containingMachine.getName())) {
 					IL1Element value = storedElements.get(nxt);
@@ -60,7 +71,8 @@ public class FMUMachineTaskingTranslator extends AbstractTaskingTranslator {
 						if (subRoutine.getMachineName().equalsIgnoreCase(
 								containingMachine.getName())
 								&& subRoutine.getName().equals(evt.getName())) {
-							// if a corresponding translation then add it to the list
+							// if a corresponding translation then add it to the
+							// list
 							// of subroutines to be added to the protected
 							subRoutineList.add(subRoutine);
 						}
@@ -70,7 +82,7 @@ public class FMUMachineTaskingTranslator extends AbstractTaskingTranslator {
 		}
 
 		// now add data to the protected object
-		if (subRoutineList.size()>0) {
+		if (subRoutineList.size() > 0) {
 			protectedObj.setMachineName(containingMachine.getName());
 			protectedObj.setName(containingMachine.getName());
 			protectedObj.setProjectName(TaskingTranslationManager.getProject()
@@ -81,7 +93,34 @@ public class FMUMachineTaskingTranslator extends AbstractTaskingTranslator {
 		else {
 			throw new TaskingTranslationUnhandledTypeException(source);
 		}
+
+		// Get, what is at the moment, the task body. Hopefully we can rename it
+		// to
+		// FMI DO STEP BODY or some such name, for FMUs.
+		EList<AbstractExtension> extensionList = containingMachine
+				.getExtensions();
+		for (AbstractExtension ext : extensionList) {
+			if (ext instanceof FMU_Machine) {
+				FMU_Machine fmuMch = (FMU_Machine) ext;
+				CompositeControl taskBody = fmuMch.getTaskBody();
+				Command b = null;
+				try {
+					b = (Command) translationManager.translate(taskBody);
+				} catch (TaskingTranslationException e) {
+					Status status = new Status(IStatus.ERROR,
+							CodeGenTasking.PLUGIN_ID,
+							"Failed Translation: TaskingTranslationException:"
+									+ e.getMessage(), e);
+					StatusManager.getManager().handle(status,
+							StatusManager.SHOW);
+				}
+				Subroutine fmiDoStep = Il1Factory.eINSTANCE.createSubroutine();
+				fmiDoStep.setBody(b);
+				// Finish of adding machine name etc......
+
+				protectedObj.getSubroutines().add(fmiDoStep);
+			}
+		}
 		return protectedObj;
 	}
-
 }
