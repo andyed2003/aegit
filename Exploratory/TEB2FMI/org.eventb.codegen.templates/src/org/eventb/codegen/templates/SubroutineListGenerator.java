1a26a2235156ec9558beae69d131e5ae14883405
andy <andy@andy-OptiPlex-755>
2013-10-24 09:27:18 +0100

    Corrected extension (exsd) to allow multiple generators. Added new generator instance for subroutine translation

---
+++ Exploratory/TEB2FMI/org.eventb.codegen.templates/src/org/eventb/codegen/templates/SubroutineListGenerator.java
@@ -0,0 +1,13 @@
+package org.eventb.codegen.templates;
+
+import java.util.List;
+
+public class SubroutineListGenerator implements IGenerator {
+
+	@Override
+	public List<String> generate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
