87c4445f4fef35d151a1235388eb09761718805e
andy <andy@andy-OptiPlex-755>
2013-10-31 10:38:59 +0000

    One change that would be added to the last commit - for some unknown reason.

---
+++ Exploratory/AtomicityDec3/templates/fmiInitializeSlave.c
@@ -0,0 +1,11 @@
+fmiStatus *fmiInitializeSlave(fmiComponent c,
+		fmiReal relativeTolerance, fmiReal tStart,
+		fmiBoolean stopTimeDefined, fmiReal tStop) {
+
+		//## <initialisationsList>
+
+		//## <stateMachineProgramCounterIni>
+
+		return fmiOK;
+
+}
