a32f621cc61ba33861cc29cb9905e2e83c4b2b77
andy <andy@andy-OptiPlex-755>
2013-11-04 12:59:14 +0000

    Updated a test/example model, which uses the FMI "functions.h". It involves casting my component to the fmiComponent (void*) type. This means we may have to change the generated code a little.

--- Examples/anFMUTest/src/environImpl.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENVIRONIMPL_H
-#define ENVIRONIMPL_H
-fmiComponent *fmiInstantiateSlave(fmiString instanceName, fmiString fmuGUID,fmiString fmuResourceLocation, const fmiCallbackFunctions* functions,fmiBoolean visible, fmiBoolean loggingOn);
-fmiStatus *fmiInitializeSlave(fmiComponent c,fmiReal relativeTolerance, fmiReal tStart,fmiBoolean stopTimeDefined, fmiReal tStop);
-fmiStatus getInteger(fmiComponent c, const fmiValueReference vr[], size_t nvr, fmiInteger value[]);
-fmiStatus fmiDoStep(fmiComponent c, fmiReal currentCommunicationPoint, fmiReal communicationStepSize, fmiBoolean noSetFMUStatePriorToCurrentPoint);
-#endif
-
