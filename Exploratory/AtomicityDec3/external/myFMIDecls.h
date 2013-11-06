dd50d3f67ba7270e477689aba93bf8747e4c9b6b
andy <andy@andy-Precision-M4700>
2013-11-06 09:51:56 +0000

    Try again...

--- Exploratory/AtomicityDec3/external/myFMIDecls.h
+++ Exploratory/AtomicityDec3/external/myFMIDecls.h
@@ -15,10 +15,10 @@ typedef struct{
 	fmiBoolean validInstance;
 	fmiString fmuInstanceName;
 	fmiString fmuGUID;
-	fmiValueReference i[integerArraySize]; // my fake fmi: array of fmiValueReferences to integers
-	fmiValueReference b[booleanArraySize]; // my fake fmi: array of fmiValueReferences to booleans
-	fmiValueReference r[realArraySize]; // my fake fmi: array of fmiValueReferences to real
-	fmiValueReference s[stringArraySize]; // my fake fmi: array of fmiValueReferences to strings
+	fmiValueReference i[integerArraySize]; // my array of fmiValueReferences to integers
+	fmiValueReference b[booleanArraySize]; // my array of fmiValueReferences to booleans
+	fmiValueReference r[realArraySize]; // my array of fmiValueReferences to real
+	fmiValueReference s[stringArraySize]; // my array of fmiValueReferences to strings
 } myfmiComponent;
 
 
