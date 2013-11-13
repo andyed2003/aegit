5c9c84b8a552000141e4fe32df6090657c5f5799
andy <andy@andy-A15A-A15HE>
2013-11-13 08:41:03 +0000

    Debug this first FMU to see if the segmentation fault appears in the exe, rather than the .so

---
+++ Examples/aCTarget2/src/common.h
@@ -0,0 +1,10 @@
+#ifndef COMMON_H
+#define COMMON_H
+#include "config.h"
+#include "fmiFunctions.h"
+#include "myFMIDecls.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "controllerImpl.h"
+#endif
+
