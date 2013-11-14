f69f5ed00c6967305e7cfb4493be4066c2e918b4
andy <andy@andy-OptiPlex-755>
2013-11-14 14:32:44 +0000

    Added more detail to the 'test' master implementation

--- Examples/aCTarget2/src/controllerImpl.h
+++ Examples/aCTarget2/src/controllerImpl.h
@@ -1,7 +1,7 @@
 #ifndef CONTROLLERIMPL_H
 #define CONTROLLERIMPL_H
-fmiValueReference a_controllerImpl_ = 0;
-fmiValueReference used_a_controllerImpl_ = 1;
+static fmiValueReference a_controllerImpl_ = 0;
+static fmiValueReference used_a_controllerImpl_ = 1;
 fmiComponent fmiInstantiateSlave(fmiString instanceName, fmiString fmuGUID,fmiString fmuResourceLocation, const fmiCallbackFunctions* functions,fmiBoolean visible, fmiBoolean loggingOn);
 fmiStatus fmiInitializeSlave(fmiComponent c,fmiReal relativeTolerance, fmiReal tStart,fmiBoolean stopTimeDefined, fmiReal tStop);
 fmiStatus fmiSetInteger(fmiComponent c, const fmiValueReference vr[], size_t nvr, const fmiInteger value[]);
--- Exploratory/AtomicityDec3/external/mymain.c
+++ Exploratory/AtomicityDec3/external/mymain.c
@@ -5,21 +5,39 @@
  *      Author: andy
  */
 
-#include <stdio.h>
-#include "fmiFunctions.h"
+#include "common.h"
+//#include <stdio.h>
+//#include "config.h"
+//#include "fmiFunctions.h"
+// How many integers to set here
+#define NUMBER_TO_SET 1
 
 int main(int argc, char **argv) {
 
+	// slave initialisation phase
 	fmiReal tolerance = 0.1;
 	fmiReal start = 0;
 	fmiReal end = 1;
-	fmiCallbackFunctions *f;
+	fmiCallbackFunctions *f = malloc(sizeof(fmiCallbackFunctions));
 	fmiComponent comp;
 	comp = fmiInstantiateSlave("a","b","",f,fmiFalse, fmiFalse);
 	fmiInitializeSlave(comp,tolerance,start,fmiTrue,end);
-	int idx = 0;
-	for(; idx < 10; idx = idx + 1){
+	// end slave initialisation
+
+	// set-up the master's initial values:
+	fmiValueReference vrs_toSet[NUMBER_TO_SET] = {a_controllerImpl_};
+	// the values set using the value references
+	fmiInteger vals_toSet[NUMBER_TO_SET] = {99};
+	// calculate some value from the environment - we use idx
+	int idx = 100;
+	// begin simulation phase
+	for(; idx < 1000; idx = idx + 100){
+		vals_toSet[0] = idx;
+		// update the controller with some value "environment" idx
+		fmiSetInteger(comp, vrs_toSet, NUMBER_TO_SET, vals_toSet );
+		// do the simulation step
 		fmiDoStep(comp,start,end,fmiTrue);
+		// we haven't define a get integer ... but it would follow here >>>
 	}
 	return 0;
 }
