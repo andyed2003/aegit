bd06667ad0107cd585ee0cb46cf3572629d93174
andyed2003 <andyed_2003@yahoo.co.uk>
2014-02-27 16:09:01 +0000

    Latest FMU shared library - using fmiInstantiate

--- Examples/UseFMU/myC.c
+++ Examples/UseFMU/myC.c
@@ -17,17 +17,33 @@ int main(int argc, char **argv) {
 	}
 
 	//>>>>>>> Get the types platform
-	// declare a pointer to the fmiFunction
-	char* (*fptr)();
+	// declare a pointer to the fmiGetTypesPlatform function
+	char* (*getfmiTypesPlatform)();
 	// get the function pointer
-	*(void **)(&fptr) = dlsym(lib_handle, "fmiGetTypesPlatform");
+	*(void **)(&getfmiTypesPlatform) = dlsym(lib_handle, "fmiGetTypesPlatform");
 	// declare a pointer to the return type
 	const char* ret;
 	// call the function and collect the return value
-	ret = (*fptr)();
-	//<<<<<<<< End of: Get the types paltform
+	ret = (*getfmiTypesPlatform)();
+	//<<<<<<<< End of: Get the types platform
 
 
+//	fmiComponent fmiInstantiateSlave(fmiString instanceName, fmiString fmuGUID,
+//			fmiString fmuResourceLocation, const fmiCallbackFunctions* functions,
+//			fmiBoolean visible, fmiBoolean loggingOn)
+
+	// declare a pointer to the fmiInstantiateSlave function
+	fmi_Component* (*fmiInstantiateSlave)(fmiString, fmiString, fmiString, const fmiCallbackFunctions*, fmiBoolean, fmiBoolean );
+//	// get the function pointer
+	*(void **)(&fmiInstantiateSlave) = dlsym(lib_handle, "fmiInstantiateSlave");
+	fmi_Component * ret2;
+	fmiString instanceName = "thisName";
+	fmiString fmuGUID = "thisGUID";
+	fmiString fmuResourceLocation = "somewhere";
+	fmiCallbackFunctions* functions;
+	fmiBoolean visible = fmiTrue;
+	fmiBoolean loggingOn = fmiFalse;
+	ret2 = (*fmiInstantiateSlave) (instanceName, fmuGUID, fmuResourceLocation, functions, visible, loggingOn);
 
 
 	const char* error_msg;
