697a8e1ab3647cb81a45a2c352573c606b569af8
andyed2003 <andyed_2003@yahoo.co.uk>
2014-03-12 17:48:55 +0000

    Added first Video Controller model

--- Examples/TestFMUPumpC_V2/Debug/src/subdir.mk
+++ Examples/TestFMUPumpC_V2/Debug/src/subdir.mk
@@ -4,20 +4,20 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../myC.c 
+../src/ControllerImpl.c 
 
 OBJS += \
-./myC.o 
+./src/ControllerImpl.o 
 
 C_DEPS += \
-./myC.d 
+./src/ControllerImpl.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.c
+src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	gcc -I/home/andy/git/aegit/Examples/TestFMUPumpC/TestFMUPumpC/external -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
