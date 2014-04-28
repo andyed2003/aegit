c2b8cb57f9aeaf1796226f3c96c43f9285c25021
andyed2003 <andyed_2003@yahoo.co.uk>
2014-04-28 16:30:06 +0100

    Adding Debug points for the Windows version of FMU V1

--- Examples/PumpControllerFMU/controllerInFMU.cmd
+++ Examples/PumpControllerFMU/controllerInFMU.cmd
@@ -35,41 +35,6 @@
     <element xmi:type="components:EventBComponent" href="default.cmp#ac.soton.fmusim.components.eventb._EQUk8L_BEeOPPJCZZo20GQ"/>
     <layoutConstraint xmi:type="notation:Bounds" xmi:id="_Kg-qs7_BEeOPPJCZZo20GQ" x="500" y="315" width="201"/>
   </children>
-  <children xmi:type="notation:Node" xmi:id="_a6srIL_BEeOPPJCZZo20GQ" type="2001">
-    <children xmi:type="notation:DecorationNode" xmi:id="_a6tSML_BEeOPPJCZZo20GQ" type="5001"/>
-    <children xmi:type="notation:BasicCompartment" xmi:id="_a6tSMb_BEeOPPJCZZo20GQ" type="7001">
-      <styles xmi:type="notation:SortingStyle" xmi:id="_a6tSMr_BEeOPPJCZZo20GQ"/>
-      <styles xmi:type="notation:FilteringStyle" xmi:id="_a6tSM7_BEeOPPJCZZo20GQ"/>
-    </children>
-    <children xmi:type="notation:Node" xmi:id="_a6zY0L_BEeOPPJCZZo20GQ" type="3001">
-      <styles xmi:type="notation:DescriptionStyle" xmi:id="_a6zY0b_BEeOPPJCZZo20GQ"/>
-      <styles xmi:type="notation:FontStyle" xmi:id="_a6zY0r_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@inputs.0"/>
-      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_a6zY07_BEeOPPJCZZo20GQ" x="15" y="98"/>
-    </children>
-    <children xmi:type="notation:Node" xmi:id="_a6z_4L_BEeOPPJCZZo20GQ" type="3001">
-      <styles xmi:type="notation:DescriptionStyle" xmi:id="_a6z_4b_BEeOPPJCZZo20GQ"/>
-      <styles xmi:type="notation:FontStyle" xmi:id="_a6z_4r_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@inputs.1"/>
-      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_a6z_47_BEeOPPJCZZo20GQ" x="60" y="98"/>
-    </children>
-    <children xmi:type="notation:Node" xmi:id="_a6z_5L_BEeOPPJCZZo20GQ" type="3002">
-      <styles xmi:type="notation:DescriptionStyle" xmi:id="_a6z_5b_BEeOPPJCZZo20GQ"/>
-      <styles xmi:type="notation:FontStyle" xmi:id="_a6z_5r_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@outputs.0"/>
-      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_a6z_57_BEeOPPJCZZo20GQ" x="130" y="98"/>
-    </children>
-    <children xmi:type="notation:Node" xmi:id="_a60m8L_BEeOPPJCZZo20GQ" type="3002">
-      <styles xmi:type="notation:DescriptionStyle" xmi:id="_a60m8b_BEeOPPJCZZo20GQ"/>
-      <styles xmi:type="notation:FontStyle" xmi:id="_a60m8r_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@outputs.1"/>
-      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_a60m87_BEeOPPJCZZo20GQ" x="175" y="98"/>
-    </children>
-    <styles xmi:type="notation:DescriptionStyle" xmi:id="_a6srIb_BEeOPPJCZZo20GQ"/>
-    <styles xmi:type="notation:FontStyle" xmi:id="_a6srIr_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-    <element xmi:type="components:FMUComponent" href="default.cmp#//@components.1"/>
-    <layoutConstraint xmi:type="notation:Bounds" xmi:id="_a6srI7_BEeOPPJCZZo20GQ" x="500" y="85"/>
-  </children>
   <children xmi:type="notation:Node" xmi:id="_piHngL_BEeOPPJCZZo20GQ" type="2003">
     <children xmi:type="notation:DecorationNode" xmi:id="_piIOkL_BEeOPPJCZZo20GQ" type="5003">
       <layoutConstraint xmi:type="notation:Location" xmi:id="_piIOkb_BEeOPPJCZZo20GQ" y="5"/>
@@ -110,28 +75,43 @@
     <element xmi:type="components:Connector" href="default.cmp#//@connectors.3"/>
     <layoutConstraint xmi:type="notation:Bounds" xmi:id="_q6koNL_BEeOPPJCZZo20GQ" x="676" y="247"/>
   </children>
+  <children xmi:type="notation:Node" xmi:id="_z0xmoM61EeOSmfRHvdImeg" type="2001">
+    <children xmi:type="notation:DecorationNode" xmi:id="_z0yNsM61EeOSmfRHvdImeg" type="5001"/>
+    <children xmi:type="notation:BasicCompartment" xmi:id="_z0yNsc61EeOSmfRHvdImeg" type="7001">
+      <styles xmi:type="notation:SortingStyle" xmi:id="_z0yNss61EeOSmfRHvdImeg"/>
+      <styles xmi:type="notation:FilteringStyle" xmi:id="_z0yNs861EeOSmfRHvdImeg"/>
+    </children>
+    <children xmi:type="notation:Node" xmi:id="_z047YM61EeOSmfRHvdImeg" type="3001">
+      <styles xmi:type="notation:DescriptionStyle" xmi:id="_z047Yc61EeOSmfRHvdImeg"/>
+      <styles xmi:type="notation:FontStyle" xmi:id="_z047Ys61EeOSmfRHvdImeg" fontName="Ubuntu"/>
+      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@inputs.0"/>
+      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_z047Y861EeOSmfRHvdImeg" x="30" y="98"/>
+    </children>
+    <children xmi:type="notation:Node" xmi:id="_z05icM61EeOSmfRHvdImeg" type="3001">
+      <styles xmi:type="notation:DescriptionStyle" xmi:id="_z05icc61EeOSmfRHvdImeg"/>
+      <styles xmi:type="notation:FontStyle" xmi:id="_z05ics61EeOSmfRHvdImeg" fontName="Ubuntu"/>
+      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@inputs.1"/>
+      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_z05ic861EeOSmfRHvdImeg" x="75" y="98"/>
+    </children>
+    <children xmi:type="notation:Node" xmi:id="_z05idM61EeOSmfRHvdImeg" type="3002">
+      <styles xmi:type="notation:DescriptionStyle" xmi:id="_z05idc61EeOSmfRHvdImeg"/>
+      <styles xmi:type="notation:FontStyle" xmi:id="_z05ids61EeOSmfRHvdImeg" fontName="Ubuntu"/>
+      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@outputs.0"/>
+      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_z05id861EeOSmfRHvdImeg" x="145" y="98"/>
+    </children>
+    <children xmi:type="notation:Node" xmi:id="_z06JgM61EeOSmfRHvdImeg" type="3002">
+      <styles xmi:type="notation:DescriptionStyle" xmi:id="_z06Jgc61EeOSmfRHvdImeg"/>
+      <styles xmi:type="notation:FontStyle" xmi:id="_z06Jgs61EeOSmfRHvdImeg" fontName="Ubuntu"/>
+      <element xmi:type="components:FMUPort" href="default.cmp#//@components.1/@outputs.1"/>
+      <layoutConstraint xmi:type="notation:Bounds" xmi:id="_z06Jg861EeOSmfRHvdImeg" x="185" y="98"/>
+    </children>
+    <styles xmi:type="notation:DescriptionStyle" xmi:id="_z0xmoc61EeOSmfRHvdImeg"/>
+    <styles xmi:type="notation:FontStyle" xmi:id="_z0xmos61EeOSmfRHvdImeg" fontName="Ubuntu"/>
+    <element xmi:type="components:FMUComponent" href="default.cmp#//@components.1"/>
+    <layoutConstraint xmi:type="notation:Bounds" xmi:id="_z0xmo861EeOSmfRHvdImeg" x="485" y="80"/>
+  </children>
   <styles xmi:type="notation:DiagramStyle" xmi:id="__FgF4b_AEeOPPJCZZo20GQ"/>
   <element xmi:type="components:ComponentDiagram" href="default.cmp#/"/>
-  <edges xmi:type="notation:Connector" xmi:id="_r_9i8L_BEeOPPJCZZo20GQ" type="4001" source="_a6zY0L_BEeOPPJCZZo20GQ" target="_piHngL_BEeOPPJCZZo20GQ">
-    <styles xmi:type="notation:FontStyle" xmi:id="_r_9i8b_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-    <element xsi:nil="true"/>
-    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_r_9i8r_BEeOPPJCZZo20GQ" points="[-2, 5, -3, -56]$[-4, 60, -5, -1]"/>
-  </edges>
-  <edges xmi:type="notation:Connector" xmi:id="_sn660L_BEeOPPJCZZo20GQ" type="4001" source="_a6z_4L_BEeOPPJCZZo20GQ" target="_p-1YsL_BEeOPPJCZZo20GQ">
-    <styles xmi:type="notation:FontStyle" xmi:id="_sn660b_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-    <element xsi:nil="true"/>
-    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_sn660r_BEeOPPJCZZo20GQ" points="[1, 5, -1, -56]$[1, 65, -1, 4]"/>
-  </edges>
-  <edges xmi:type="notation:Connector" xmi:id="_tMjR4L_BEeOPPJCZZo20GQ" type="4001" source="_a6z_5L_BEeOPPJCZZo20GQ" target="_qW3eML_BEeOPPJCZZo20GQ">
-    <styles xmi:type="notation:FontStyle" xmi:id="_tMjR4b_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-    <element xsi:nil="true"/>
-    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_tMjR4r_BEeOPPJCZZo20GQ" points="[5, 5, 2, -59]$[7, 63, 4, -1]"/>
-  </edges>
-  <edges xmi:type="notation:Connector" xmi:id="_t5pBYL_BEeOPPJCZZo20GQ" type="4001" source="_a60m8L_BEeOPPJCZZo20GQ" target="_q6koML_BEeOPPJCZZo20GQ">
-    <styles xmi:type="notation:FontStyle" xmi:id="_t5pBYb_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
-    <element xsi:nil="true"/>
-    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_t5pBYr_BEeOPPJCZZo20GQ" points="[0, 5, 0, -58]$[1, 66, 1, 3]"/>
-  </edges>
   <edges xmi:type="notation:Connector" xmi:id="_vTM1UL_BEeOPPJCZZo20GQ" type="4001" source="_KhAf5L_BEeOPPJCZZo20GQ" target="_q6koML_BEeOPPJCZZo20GQ">
     <styles xmi:type="notation:FontStyle" xmi:id="_vTM1Ub_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
     <element xsi:nil="true"/>
@@ -151,5 +131,25 @@
     <styles xmi:type="notation:FontStyle" xmi:id="_yEl-4L_BEeOPPJCZZo20GQ" fontName="Ubuntu"/>
     <element xsi:nil="true"/>
     <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_yEl-4b_BEeOPPJCZZo20GQ" points="[0, -5, -1, 57]$[0, -58, -1, 4]"/>
+  </edges>
+  <edges xmi:type="notation:Connector" xmi:id="__uUAcM61EeOSmfRHvdImeg" type="4001" source="_z06JgM61EeOSmfRHvdImeg" target="_q6koML_BEeOPPJCZZo20GQ">
+    <styles xmi:type="notation:FontStyle" xmi:id="__uUAcc61EeOSmfRHvdImeg" fontName="Ubuntu"/>
+    <element xsi:nil="true"/>
+    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="__uUAcs61EeOSmfRHvdImeg" points="[4, 5, -1, -63]$[4, 63, -1, -5]"/>
+  </edges>
+  <edges xmi:type="notation:Connector" xmi:id="_A-kucM62EeOSmfRHvdImeg" type="4001" source="_z05idM61EeOSmfRHvdImeg" target="_qW3eML_BEeOPPJCZZo20GQ">
+    <styles xmi:type="notation:FontStyle" xmi:id="_A-kucc62EeOSmfRHvdImeg" fontName="Ubuntu"/>
+    <element xsi:nil="true"/>
+    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_A-kucs62EeOSmfRHvdImeg" points="[2, 5, -1, -64]$[2, 73, -1, 4]"/>
+  </edges>
+  <edges xmi:type="notation:Connector" xmi:id="_ClvgkM62EeOSmfRHvdImeg" type="4001" source="_z05icM61EeOSmfRHvdImeg" target="_p-1YsL_BEeOPPJCZZo20GQ">
+    <styles xmi:type="notation:FontStyle" xmi:id="_Clvgkc62EeOSmfRHvdImeg" fontName="Ubuntu"/>
+    <element xsi:nil="true"/>
+    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_Clvgks62EeOSmfRHvdImeg" points="[2, 5, 0, -61]$[4, 68, 2, 2]"/>
+  </edges>
+  <edges xmi:type="notation:Connector" xmi:id="_DVp6kM62EeOSmfRHvdImeg" type="4001" source="_z047YM61EeOSmfRHvdImeg" target="_piHngL_BEeOPPJCZZo20GQ">
+    <styles xmi:type="notation:FontStyle" xmi:id="_DVp6kc62EeOSmfRHvdImeg" fontName="Ubuntu"/>
+    <element xsi:nil="true"/>
+    <bendpoints xmi:type="notation:RelativeBendpoints" xmi:id="_DVp6ks62EeOSmfRHvdImeg" points="[1, 5, 0, -61]$[3, 62, 2, -4]"/>
   </edges>
 </notation:Diagram>
