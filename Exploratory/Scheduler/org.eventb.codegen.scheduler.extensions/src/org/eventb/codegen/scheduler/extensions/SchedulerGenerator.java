efd7c00df2e513bb9c35b6693dea9d01c2c07d8f
andyed_2003@yahoo.co.uk <andyed_2003@yahoo.co.uk>
2014-06-10 09:41:19 +0100

    Added new Scheduler Project

---
+++ Exploratory/Scheduler/org.eventb.codegen.scheduler.extensions/src/org/eventb/codegen/scheduler/extensions/SchedulerGenerator.java
@@ -0,0 +1,17 @@
+package org.eventb.codegen.scheduler.extensions;
+
+import java.util.List;
+
+import org.eventb.codegen.templates.IGenerator;
+import org.eventb.codegen.templates.IGeneratorData;
+
+public class SchedulerGenerator implements IGenerator {
+
+	@Override
+	public List<String> generate(IGeneratorData data) throws Exception {
+		// unpack
+		System.out.println("Get Here");
+		return null;
+	}
+
+}
