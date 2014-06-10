6e0af77f8ccec171a425d7ae177f7695600a1a63
andyed_2003@yahoo.co.uk <andyed_2003@yahoo.co.uk>
2014-06-10 10:41:59 +0100

    added scheduler Event-B to IL1 skeleton

---
+++ Exploratory/Scheduler/org.eventb.codegen.scheduler.extensions/src/org/eventb/codegen/scheduler/extensions/TranslateEventBToIL1.java
@@ -0,0 +1,19 @@
+package org.eventb.codegen.scheduler.extensions;
+
+import org.eventb.codegen.il1.IL1Element;
+import org.eventb.codegen.tasking.AbstractTaskingTranslator;
+import org.eventb.codegen.tasking.TaskingTranslationException;
+import org.eventb.codegen.tasking.TaskingTranslationManager;
+import org.eventb.emf.core.EventBElement;
+
+public class TranslateEventBToIL1 extends AbstractTaskingTranslator {
+
+	@Override
+	public IL1Element translate(EventBElement source, IL1Element target,
+			TaskingTranslationManager translationManager)
+			throws TaskingTranslationException {
+		
+		return null;
+	}
+
+}
