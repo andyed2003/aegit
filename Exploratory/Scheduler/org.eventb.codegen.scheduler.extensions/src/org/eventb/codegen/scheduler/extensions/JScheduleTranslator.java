7d2299dae75318afc13a2d52cf0e9bd4904e948e
andyed2003 <andyed_2003@yahoo.co.uk>
2014-06-18 10:54:53 +0100

    JScheduler development

--- Exploratory/Scheduler/org.eventb.codegen.scheduler.extensions/src/org/eventb/codegen/scheduler/extensions/JScheduleTranslator.java
+++ Exploratory/Scheduler/org.eventb.codegen.scheduler.extensions/src/org/eventb/codegen/scheduler/extensions/JScheduleTranslator.java
@@ -5,9 +5,15 @@ import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eventb.codegen.il1.Command;
 import org.eventb.codegen.il1.IL1Element;
+import org.eventb.codegen.il1.If;
 import org.eventb.codegen.il1.Il1Factory;
+import org.eventb.codegen.il1.MainEntry;
+import org.eventb.codegen.il1.Program;
+import org.eventb.codegen.il1.Seq;
 import org.eventb.codegen.il1.Subroutine;
+import org.eventb.codegen.il1.Task;
 import org.eventb.codegen.il1.translator.IL1TranslationManager;
 import org.eventb.codegen.il1.translator.IL1TranslationUnhandledTypeException;
 import org.eventb.codegen.il1.translator.TargetLanguage;
@@ -29,27 +35,58 @@ public class JScheduleTranslator {
 	public void translate(IStructuredSelection s) throws RodinDBException,
 			TaskingTranslationException, IL1TranslationUnhandledTypeException {
 		Object[] list = s.toArray();
-//		RMLDataStruct relevantMachines = RelevantMachineLoader
-//				.getAllRelevantMachines(list);
+// Set up the target language, and load in the machine
 		TargetLanguage targetLanguage = new TargetLanguage("Java");
 		MachineRoot firstMachine = (MachineRoot) list[0];
 		MachineImpl mch = RodinToEMFConverter.loadMachine(firstMachine);
+// Get the events
 		EList<Event> eventList = mch.getEvents();		
-		
+// Set up the translation managers	
 		Il1Factory factory = Il1Factory.eINSTANCE;
 		TaskingTranslationManager taskingTranslationManager = new TaskingTranslationManager(factory);
 		IL1TranslationManager il1TranslationManager = new IL1TranslationManager();
-		
-		List<Subroutine> subroutineList = new ArrayList<Subroutine>();
+// Each event should map to branch - put the events in a 'DUMMY' Task and Body
+		Task dummyTask = Il1Factory.eINSTANCE.createTask();
+		Command dummyCode = dummyTask.getBody();
+		boolean firstStatement = true;
+// The task is used under the covers by the code generator - but not reproduced.
+		dummyTask.setMachineName(firstMachine.getElementName());
+		dummyTask.setProjectName(firstMachine.getEventBProject().getRodinProject().getElementName());
+		List<If> branchList = new ArrayList<If>();
+// iterate through the events		
 		for(Event evt: eventList){
 			Subroutine subroutine = (Subroutine) taskingTranslationManager.translate(evt);
-			subroutineList.add(subroutine);
+			if(subroutine != null){
+				// create a new branch and add guards and statements
+				If newBranch = Il1Factory.eINSTANCE.createIf();
+				EList<String> branchConditions = newBranch.getCondition();
+				//?? WE HAVE TO MARK THE CONTAINER SOMEWHERE
+				// SO IT IS PICKED UP LATER >>> OR SET MACHINE AND PROJECT NAME
+				// OF EACH ELEMENT.
+				branchConditions.addAll(subroutine.getGuards());
+				newBranch.setBody(subroutine.getBody());
+				newBranch.setParentEventName(evt.getName());
+				Seq newSeq = Il1Factory.eINSTANCE.createSeq();
+				Seq previousSeq = newSeq;
+				if(firstStatement){
+					// add the branch to the dummy task body
+					firstStatement = false;
+					newSeq.setLeftBranch(newBranch);
+					dummyCode = newBranch;
+				}
+				else{
+					// add branch to the rhs of the last seq
+					newSeq.setLeftBranch(newBranch);
+					previousSeq.setRightBranch(newSeq);
+				}
+				branchList.add(newBranch);
+			}
 		}
 		
 		
 		ArrayList<String> outcode = new ArrayList<String>();
-		for(Subroutine subroutine: subroutineList){
-			ArrayList<String> code = il1TranslationManager.translateIL1ElementToCode(subroutine, targetLanguage);
+		for(If branch: branchList){
+			ArrayList<String> code = il1TranslationManager.translateIL1ElementToCode(branch, targetLanguage);
 			outcode.addAll(code);
 		}
 		System.out.println();
