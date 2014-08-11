d5ce22a8747408b2569c27e8f964d4536bc317cd
andyed2003 <andyed_2003@yahoo.co.uk>
2014-08-11 16:22:30 +0100

    Updated XText generated projects/files. Added/Update Example usage of Event-B text. Added 'TestAccessEMF' project. To test access to the underlying EMF. Can also access the model using the built-in 'sample reflective ecore editor'.

--- Exploratory/XText/ac.soton.xtext.context.dsl/src-gen/ac/soton/xtext/context/dsl/ContextDslStandaloneSetupGenerated.java
+++ Exploratory/XText/ac.soton.xtext.context.dsl/src-gen/ac/soton/xtext/context/dsl/ContextDslStandaloneSetupGenerated.java
@@ -32,8 +32,8 @@ public class ContextDslStandaloneSetupGenerated implements ISetup {
 
 		org.eclipse.xtext.resource.IResourceFactory resourceFactory = injector.getInstance(org.eclipse.xtext.resource.IResourceFactory.class);
 		org.eclipse.xtext.resource.IResourceServiceProvider serviceProvider = injector.getInstance(org.eclipse.xtext.resource.IResourceServiceProvider.class);
-		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("mydsl", resourceFactory);
-		org.eclipse.xtext.resource.IResourceServiceProvider.Registry.INSTANCE.getExtensionToFactoryMap().put("mydsl", serviceProvider);
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ctx", resourceFactory);
+		org.eclipse.xtext.resource.IResourceServiceProvider.Registry.INSTANCE.getExtensionToFactoryMap().put("ctx", serviceProvider);
 		
 
 
---
+++ Exploratory/XText/testAccessEMF/src/testaccessemf/popup/actions/NewAction.java
@@ -0,0 +1,78 @@
+package testaccessemf.popup.actions;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IActionDelegate;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.xtext.internal.ResourceServiceProviderDescriptor;
+import org.eclipse.xtext.resource.IResourceServiceProvider;
+import org.eclipse.xtext.resource.IResourceServiceProvider.Registry;
+import org.eventb.emf.core.CorePackage;
+import org.eventb.emf.core.machine.Machine;
+import org.eventb.emf.core.machine.MachinePackage;
+import org.eventb.emf.core.machine.impl.MachineImpl;
+
+
+public class NewAction implements IObjectActionDelegate {
+	@SuppressWarnings("unused")
+	private Shell shell;
+	private IStructuredSelection selection;
+
+	/**
+	 * Constructor for Action1.
+	 */
+	public NewAction() {
+		super();
+	}
+
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		shell = targetPart.getSite().getShell();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		
+		IFile file = (IFile) selection.getFirstElement();
+		String path = file.getFullPath().toOSString();
+		
+		ResourceSet rs = new ResourceSetImpl();
+		URI uri = URI.createPlatformResourceURI(path, true);
+		Resource r = rs.getResource(uri, true);
+		List<EObject> contentOfYourFile = r.getContents();
+		for(EObject eo: contentOfYourFile){
+			Class<? extends EObject> clazz = eo.getClass();
+			if(clazz.equals(MachineImpl.class) ){
+				Machine m = (Machine) eo;
+				System.out.println("We found the EMF model of Machine: "+ m.getName());
+			}
+		}
+		
+		
+		
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		this.selection = (IStructuredSelection) selection;
+	}
+
+}
