b049caddcca9cbff73bf0c63acb12dd69acff6c4
andyed2003 <andyed_2003@yahoo.co.uk>
2014-09-25 14:40:34 +0100

    updated xtext examples

--- Exploratory/XText/testAccessEMF/src/testaccessemf/popup/actions/NewAction.java
+++ Exploratory/XText/testAccessEMF/src/testaccessemf/popup/actions/NewAction.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -20,6 +22,8 @@ import org.eclipse.xtext.internal.ResourceServiceProviderDescriptor;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.resource.IResourceServiceProvider.Registry;
 import org.eventb.emf.core.CorePackage;
+import org.eventb.emf.core.machine.Event;
+import org.eventb.emf.core.machine.Invariant;
 import org.eventb.emf.core.machine.Machine;
 import org.eventb.emf.core.machine.MachinePackage;
 import org.eventb.emf.core.machine.impl.MachineImpl;
@@ -55,11 +59,16 @@ public class NewAction implements IObjectActionDelegate {
 		ResourceSet rs = new ResourceSetImpl();
 		URI uri = URI.createPlatformResourceURI(path, true);
 		Resource r = rs.getResource(uri, true);
+		
 		List<EObject> contentOfYourFile = r.getContents();
 		for(EObject eo: contentOfYourFile){
 			Class<? extends EObject> clazz = eo.getClass();
 			if(clazz.equals(MachineImpl.class) ){
 				Machine m = (Machine) eo;
+				EList<Invariant> invariants = m.getInvariants();
+				EList<Event> events = m.getEvents();
+				 EList<EObject> iter = m.eContents();
+				
 				
 				System.out.println("We found the EMF model of Machine: "+ m.getName());
 			}
